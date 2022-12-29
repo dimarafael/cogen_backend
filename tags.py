28d819ca3a5413ffef3948f4318927f0482a19a5
dimarafael <dimarafael@gmail.com>
2022-12-29 11:40:16 +0100

    tags

--- tags.py
+++ tags.py
@@ -1,19 +1,111 @@
 TAGS_BOOL = {
     'drum': 112,
-    'mixer': 113
+    'mixer': 113,
+    'cooler': 119,
+    'fire': 122
 }
 
 TAGS_NUMBER = {
+    'sp_max_t': {
+        'address': 106,
+        'min': 1,
+        'max': 500,
+        'type': float
+    },
+    'sp_dp_min': {
+        'address': 110,
+        'min': 1,
+        'max': 100,
+        'type': float
+    },
+    'drum_speed': {
+        'address': 114,
+        'min': 1,
+        'max': 100,
+        'type': float
+    },
+    'smoke_fan_speed': {
+        'address': 117,
+        'min': 1,
+        'max': 100,
+        'type': float
+    },
+    'cooler_speed': {
+        'address': 120,
+        'min': 1,
+        'max': 100,
+        'type': float
+    },
+    'gaz_start': {
+        'address': 146,
+        'min': 1,
+        'max': 100,
+        'type': float
+    },
+    'gaz1': {
+        'address': 148,
+        'min': 1,
+        'max': 100,
+        'type': float
+    },
+    'gaz2': {
+        'address': 150,
+        'min': 1,
+        'max': 100,
+        'type': float
+    },
+    'gaz3': {
+        'address': 152,
+        'min': 1,
+        'max': 100,
+        'type': float
+    },
+    'gaz4': {
+        'address': 154,
+        'min': 1,
+        'max': 100,
+        'type': float
+    },
+    'gaz5': {
+        'address': 156,
+        'min': 1,
+        'max': 100,
+        'type': float
+    },
+    'gaz6': {
+        'address': 158,
+        'min': 1,
+        'max': 100,
+        'type': float
+    },
+    'gaz7': {
+        'address': 160,
+        'min': 1,
+        'max': 100,
+        'type': float
+    },
+    'gaz8': {
+        'address': 162,
+        'min': 1,
+        'max': 100,
+        'type': float
+    },
+    'gaz9': {
+        'address': 164,
+        'min': 1,
+        'max': 100,
+        'type': float
+    },
+    'gaz10': {
+        'address': 166,
+        'min': 1,
+        'max': 100,
+        'type': float
+    },
     'gaz_preset': {
         'address': 168,
         'min': 1,
         'max': 10,
         'type': int
-    },
-    'smoke_fan_speed': {
-        'address': 117,
-        'min': 1.0,
-        'max': 100.0,
-        'type': float
     }
 }
